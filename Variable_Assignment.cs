1ba3b22eedefcba38c13974cded74b2902863d55
SammyRosiePosie <122414886+SammyRosiePosie@users.noreply.github.com>
2023-01-21 20:29:24 -0700

    Completed Variable Assignment
    Dating Sim idea for game. Potential characters

---
+++ Variable_Assignment.cs
@@ -0,0 +1,50 @@
+using System;
+
+namespace matchingSystem1
+{
+  class program
+  {
+  
+    static void Main(string[] args)
+    {
+      // [data-type] [identifier] [assignment operator] [value]
+      // Variable Declaration
+      string subjectMale = "Male subject:";
+      string firstNameM = "George";
+      char middleInM = 'M';
+      char lastInM = 'F';
+      int ageM = 35;
+      double heightM = 5.83f;
+      float weightM = 185.2f;
+      bool relationshipM = false;
+      
+      Console.WriteLine(subjectMale);
+      Console.WriteLine(firstNameM);
+      Console.WriteLine(middleInM);
+      Console.WriteLine(lastInM);
+      Console.WriteLine(ageM);
+      Console.WriteLine(heightM);
+      Console.WriteLine(weightM);
+      Console.WriteLine(relationshipM);
+      
+      string subjectFemale = "Female subject:";
+      string firstNameF = "Rebecca";
+      char middleInF = 'N';
+      char lastInF = 'T';
+      int ageF = 28;
+      double heightF = 6f;
+      float weightF = 190.3f;
+      bool relationshipF = false;
+      
+      Console.WriteLine(subjectFemale);
+      Console.WriteLine(firstNameF);
+      Console.WriteLine(middleInF);
+      Console.WriteLine(lastInF);
+      Console.WriteLine(ageF);
+      Console.WriteLine(heightF);
+      Console.WriteLine(weightF);
+      Console.WriteLine(relationshipF);
+      
+      }
+    }
+  }

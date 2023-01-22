5c829e189bd420065b68a8b6971277f6b928c4b3
SammyRosie <10873132@uvu.edu>
2023-01-21 22:19:02 -0700

    Started inclass assignment but had to sleep in between

---
+++ First Prototype/Assets/Scripts/Variables.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    //[Access Moddifier] [Data-Type/ Type] [Identifier/Name] [Access operator =] [Value]
+
+    public string herosName = "Crimson Wings";
+
+    private string herosRealName = "Tommy";
+
+    public char heroLetter = 'C';
+
+    private int herosAge = 16;
+
+    public float heroHeight = 6.2f;
+
+    private double heroNetWorth = 50.00d;
+
+    public bool heroInvincibility = false;
+
+    public string herosPower = "Healing, speed";
+
+    //Stopped at 47 mins in video
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
